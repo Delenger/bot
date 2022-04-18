789eb0e4c7b2c9ecb578e617b77fa79988022daf
Delenger <103952815+Delenger@users.noreply.github.com>
2022-04-18 19:53:12 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     TOKEN: "5362698613:AAGBhuMUPmIxxnOw5srVpeDTI4J6zNZiKnc",
     BOT_LOGIN: "test1289378947123_bot",
+    PORT: 80,
     database: {
         database: "default_db",
         username: "gen_user",
