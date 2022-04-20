dbbd7956de8f26d170bcec335b6114c9dc12289d
danil.jolorvs.23213 <danil.jolorvs.23213@mail.ru>
2022-04-21 00:54:29 +0300

    fix config

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  TOKEN: "5371476287:AAHW7opLJoIH5vQJrLg6r9uxUTXYx4FdClo", // "5228300815:AAFi0I_4TrCh_rv2Ha4U0cjOvSioOPcJ_Y4",
-  BOT_LOGIN: "test235254_bot", // "lcdp_ro_bot",
-  PORT: 3000, //80,
+  TOKEN: "5228300815:AAFi0I_4TrCh_rv2Ha4U0cjOvSioOPcJ_Y4",
+  BOT_LOGIN: "lcdp_ro_bot",
+  PORT: 80,
   database: {
-    database: "bot", //"default_db",
-    username: "root", //"gen_user",
-    password: "", //"xsna1jzai10",
+    database: "default_db",
+    username: "gen_user",
+    password: "xsna1jzai10",
   },
 };
 
