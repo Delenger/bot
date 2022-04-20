fd646f93813895fe20ad528d2b5e3fb4910f3865
danil.jolorvs.23213 <danil.jolorvs.23213@mail.ru>
2022-04-20 03:25:09 +0300

    fix cinfig

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  TOKEN: "5371476287:AAHW7opLJoIH5vQJrLg6r9uxUTXYx4FdClo", //"5362698613:AAGBhuMUPmIxxnOw5srVpeDTI4J6zNZiKnc",
+  TOKEN: "5362698613:AAGBhuMUPmIxxnOw5srVpeDTI4J6zNZiKnc",
   BOT_LOGIN: "test1289378947123_bot",
-  PORT: 3000, //80,
+  PORT: 80,
   database: {
-    database: "bot", // "default_db",
-    username: "root", // "gen_user",
-    password: "", // "cogfwzmts10",
+    database: "default_db",
+    username: "gen_user",
+    password: "cogfwzmts10",
   },
 };
 
