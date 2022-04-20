78bb39125cac6089ea8dbe5abaa829574c0f7022
danil.jolorvs.23213 <danil.jolorvs.23213@mail.ru>
2022-04-20 13:02:20 +0300

    fix

--- commands/requests.js
+++ commands/requests.js
@@ -9,11 +9,14 @@ module.exports = async (ctx) => {
         parse_mode: "HTML",
         reply_markup: Markup.inlineKeyboard([[Markup.callbackButton(locale.requests.ready_send_button, "send_request")]]),
       })
-      .catch((err) =>
-        ctx.replyOrEdit(locale.requests.need_send_request, {
-          parse_mode: "HTML",
-          reply_markup: Markup.inlineKeyboard([[Markup.callbackButton(locale.requests.ready_send_button, "send_request")]]),
-        })
+      .catch(
+        (err) => {
+          console.log("error");
+        }
+        // ctx.replyOrEdit(locale.requests.need_send_request, {
+        //   parse_mode: "HTML",
+        //   reply_markup: Markup.inlineKeyboard([[Markup.callbackButton(locale.requests.ready_send_button, "send_request")]]),
+        // })
       );
   } catch {
     console.log("Error");
