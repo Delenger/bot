e87e16a7a1dff376825e17e7c5a13ccfa21e9e50
danil.jolorvs.23213 <danil.jolorvs.23213@mail.ru>
2022-04-20 13:19:02 +0300

    fix

--- commands/requests.js
+++ commands/requests.js
@@ -3,12 +3,12 @@ const locale = require("../locale");
 
 module.exports = async (ctx) => {
   try {
-    return ctx
-      .replyWithPhoto("https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/445fd962883585.5a9ed87691b84.jpg", {
-        caption: locale.requests.need_send_request,
-        parse_mode: "HTML",
-        reply_markup: Markup.inlineKeyboard([[Markup.callbackButton(locale.requests.ready_send_button, "send_request")]]),
-      })
+    ctx.replyWithPhoto("https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/445fd962883585.5a9ed87691b84.jpg", {
+      caption: locale.requests.need_send_request,
+      parse_mode: "HTML",
+      reply_markup: Markup.inlineKeyboard([[Markup.callbackButton(locale.requests.ready_send_button, "send_request")]]),
+    });
+    /*
       .catch(
         ctx
           .replyOrEdit(locale.requests.need_send_request, {
@@ -17,6 +17,7 @@ module.exports = async (ctx) => {
           })
           .catch((err) => err)
       );
+      */
   } catch {
     console.log("Error");
   }
