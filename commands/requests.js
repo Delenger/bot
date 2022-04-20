50a93895d19798a89acbc4ea309be314c5101d47
danil.jolorvs.23213 <danil.jolorvs.23213@mail.ru>
2022-04-20 13:21:42 +0300

    fix

--- commands/requests.js
+++ commands/requests.js
@@ -2,13 +2,14 @@ const { Markup } = require("telegraf");
 const locale = require("../locale");
 
 module.exports = async (ctx) => {
-  try {
-    ctx.replyWithPhoto("https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/445fd962883585.5a9ed87691b84.jpg", {
+  ctx
+    .replyWithPhoto("https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/445fd962883585.5a9ed87691b84.jpg", {
       caption: locale.requests.need_send_request,
       parse_mode: "HTML",
       reply_markup: Markup.inlineKeyboard([[Markup.callbackButton(locale.requests.ready_send_button, "send_request")]]),
-    });
-    /*
+    })
+    .catch((err) => err);
+  /*
       .catch(
         ctx
           .replyOrEdit(locale.requests.need_send_request, {
@@ -18,7 +19,4 @@ module.exports = async (ctx) => {
           .catch((err) => err)
       );
       */
-  } catch {
-    console.log("Error");
-  }
 };
